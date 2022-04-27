b7b462daa800772b6dbdfaa7e56b5881c7f0e2ca
Simon <Simon@FONDK-6E5L6TS8L>
2022-04-27 14:07:20 +0200

    Add project files.

---
+++ H1_VIndhastighed/Program.cs
@@ -0,0 +1,60 @@
+﻿
+static void Main(string[] args)
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    string menu = "";
+
+    Console.SetCursorPosition(20, 2);
+    Console.WriteLine("Hovedemenu");
+    Console.SetCursorPosition(20, 3);
+    Console.WriteLine("--------------------------------");
+    Console.SetCursorPosition(20, 4);
+    Console.WriteLine("Tryk 1 For Konvertere km/t");
+    Console.SetCursorPosition(20, 5);
+    Console.WriteLine("Tryk 2 For Konvertere knots");
+    Console.SetCursorPosition(20, 6);
+    Console.WriteLine("Tryk 3 For Konvertere mph");
+    Console.SetCursorPosition(20, 7);
+    Console.WriteLine("Tryk 4 For Konvertere m/s");
+    Console.ReadKey();
+
+    switch (menu)
+    {
+
+        case "1":
+            {
+                Console.WriteLine("Indtast km/t");
+                int kmtt = Convert.ToInt32(Console.ReadLine());
+            }
+            break;
+        case "2":
+            {
+                Console.WriteLine("Knovertering Complete");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            break;
+        case "3":
+            {
+                Console.WriteLine("Knovertering Complete");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            break;
+        case "4":
+            {
+                Console.WriteLine("Knovertering Complete");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            break;
+
+    }
+
+}
---
+++ TestArea/Program.cs
@@ -0,0 +1,30 @@
+﻿// C# program for the conversion
+// kmph to mph and vice versa
+using System;
+
+class GFG
+{
+
+    // Function to convert kmph to mph
+    static double kmphTOmph(double kmph)
+    {
+        return 0.6214 * kmph;
+    }
+
+    // Function to convert mph to kmph
+    static double mphTOkmph(double mph)
+    {
+        return mph * 1.60934;
+    }
+
+    // Driver code to check the above function
+    public static void Main()
+    {
+        double kmph = 200;
+        double mph = 100;
+        Console.WriteLine("speed in miles/hr is " + kmphTOmph(kmph));
+        Console.WriteLine("speed in km/hr is " + mphTOkmph(mph));
+    }
+}
+
+// This code is contributed by vt_m
